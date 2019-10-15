cf5394d77860b52b4d4c85de0dd47425c46107d9
Artyom Grishchenko <artyomgrishchenko@gmail.com>
2019-10-15 10:03:49 +0300

    fixed names assembly

--- release.ps1
+++ release.ps1
@@ -4,27 +4,27 @@ Set-StrictMode -Version latest
 $ErrorActionPreference = "Stop"
 
 $component = Get-Content -Path "component.json" | ConvertFrom-Json
-[xml]$xml = Get-Content -Path src/Interface/Wexxle.Guide.Interface.csproj
+[xml]$xml = Get-Content -Path src/Interface/Interface.csproj
 $version = $xml.Project.PropertyGroup.Version
 
 if ($component.version -ne $version) {
-    throw "Versions in component.json and Wexxle.Guide.Interface.csproj do not match"
+    throw "Versions in component.json and Interface.csproj do not match"
 }
 
-[xml]$xml2 = Get-Content -Path src/Client/Wexxle.Guide.Client.csproj
+[xml]$xml2 = Get-Content -Path src/Client/Client.csproj
 $version = $xml2.Project.PropertyGroup.Version
 
 if ($component.version -ne $version) {
-    throw "Versions in component.json and Wexxle.Guide.Client.csproj do not match"
+    throw "Versions in component.json and Client.csproj do not match"
 }
 
 # # Build Interface package
-# dotnet build src/Interface/Wexxle.Guide.Interface.csproj -c Release
-# dotnet pack src/Interface/Wexxle.Guide.Interface.csproj -c Release -o ../../dist
+# dotnet build src/Interface/Interface.csproj -c Release
+# dotnet pack src/Interface/Interface.csproj -c Release -o ../../dist
 
 # # Build Client package
-# dotnet build src/Client/Wexxle.Guide.Client.csproj -c Release
-# dotnet pack src/Client/Wexxle.Guide.Client.csproj -c Release -o ../../dist
+# dotnet build src/Client/Client.csproj -c Release
+# dotnet pack src/Client/Client.csproj -c Release -o ../../dist
 
 $packages = (Get-ChildItem -Path "dist/*.$version.nupkg")
 
