cf6837c7008e7cceb415cd71b88515d4b95381b1
Artyom Grishchenko <artyomgrishchenko@gmail.com>
2019-10-16 22:59:33 +0300

    fixed client factory

--- src/Service/Build/GuidesClientFactory.cs
+++ src/Service/Build/GuidesClientFactory.cs
@@ -10,15 +10,11 @@ namespace Wexxle.Guide.Build
         public static Descriptor NullClientDescriptor = new Descriptor("wexxle-guides", "client", "null", "*", "1.0");
         public static Descriptor DirectClientDescriptor = new Descriptor("wexxle-guides", "client", "direct", "*", "1.0");
         public static Descriptor HttpClientDescriptor = new Descriptor("wexxle-guides", "client", "http", "*", "1.0");
-
-		public static Descriptor AttachmentsDescriptor = new Descriptor("wexxle-attachments", "client", "http", "*", "1.0");
 		public GuidesClientFactory()
         {
             RegisterAsType(GuidesClientFactory.NullClientDescriptor, typeof(GuidesNullClientV1));
             RegisterAsType(GuidesClientFactory.DirectClientDescriptor, typeof(GuidesDirectClientV1));
             RegisterAsType(GuidesClientFactory.HttpClientDescriptor, typeof(GuidesHttpClientV1));
-
-			RegisterAsType(GuidesClientFactory.AttachmentsDescriptor, typeof(AttachmentsHttpClientV1));
 		}
     }
 }
